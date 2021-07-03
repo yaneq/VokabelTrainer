22dc8aa6be0dfd68e0fcd9c7e2cefe9e877e478c
Jan Koeppen <jan@yaneq.com>
2021-07-03 12:59:29 -0300

    initial commit

---
+++ src/models/WordPairs.ts
@@ -0,0 +1,14 @@
+import { iWordPair } from '../types/types'
+
+const WORD_PAIRS = [
+  ['los cubiertos', 'das Besteck'],
+  ['los platos', 'die Teller'],
+  ['gustar', 'mögen'],
+  ['colgar', 'aufhängen'],
+  ['aspirar', 'staubsaugen'],
+  ['podar', 'Rasen mähen]'],
+]
+
+export const getRandomWordPair = (): iWordPair => {
+  return
+}
---
+++ src/types/types.ts
@@ -0,0 +1,4 @@
+export interface iWordPair {
+  spanish: string;
+  german: string;
+}
