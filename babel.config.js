3f5141c8b9b4163b9710258714cfb65bca3af3a7
Jan Koeppen <jan@yaneq.com>
2021-07-04 18:16:08 -0300

    add custom fonts

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@types': './src/types',
+          '@lib': './src/lib',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+}
